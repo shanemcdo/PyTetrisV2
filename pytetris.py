b2583e6b984a3c91a8703d39537e86b55c1458b3
Shane McDonough <shanemcdman@gmail.com>
2021-01-08 00:17:46 -0800

    Allow for main menu to open a new screen and come back to main later

--- pytetris.py
+++ pytetris.py
@@ -8,6 +8,20 @@ from game_screens import Point, Button, GameScreen, MenuScreen
 def new_matrix(width: int, height: int = None, value = None) -> [[]]:
     return [[value for j in range(width)] for i in range(height if height else width)]
 
+class PyTetrisGame(GameScreen):
+    """
+    The pytetris game itself.
+    runs the game logic
+    """
+
+    def __init__(self, screen: pygame.Surface, window_size: Point):
+        super().__init__(screen, window_size, 60)
+
+    def key_down(self, event: pygame.event.Event):
+        if event.key == K_ESCAPE:
+            self.running = False
+
+
 class MainMenu(MenuScreen):
     """The main menu of the pytetris game"""
 
@@ -16,19 +30,24 @@ class MainMenu(MenuScreen):
         # lucidaconsole, lucidasans, agencyfb, copperplategothic, dubairegualar
         # font = pygame.font.SysFont('lucidaconsole', 60)
         font = pygame.font.Font('assets/tetris-atari.ttf', 30)
+        game = PyTetrisGame(screen, window_size)
         self.buttons = [
-            Button(lambda: print('Play'), 'Play', Rect(40, 190, 260, 100), font, border_size = 2),
+            Button(game.run, 'Play', Rect(40, 190, 260, 100), font, border_size = 2),
             Button(lambda: print('Controls'), 'Controls', Rect(40, 300, 260, 100), font, border_size = 2),
             Button(lambda: print('Options'), 'Options', Rect(40, 410, 260, 100), font, border_size = 2),
             Button(sys.exit, 'Quit', Rect(40, 520, 260, 100), font, border_size = 2),
             ]
         # TODO: Create acutally good background / title <07-01-21, ShaneMcDonough>
-        background = pygame.image.load('assets/menu_background.png')
-        background_rect = background.get_rect()
-        background_rect.y = window_size.y - background_rect.h
-        screen.blit(background, background_rect)
-        title = pygame.image.load('assets/Title.png')
-        screen.blit(title, (50, 50))
+        self.background = pygame.image.load('assets/menu_background.png')
+        self.background_rect = self.background.get_rect()
+        self.background_rect.y = self.window_size.y - self.background_rect.h
+        self.title = pygame.image.load('assets/Title.png')
+
+    def update(self):
+        self.screen.fill((0, 0, 0))
+        screen.blit(self.background, self.background_rect)
+        screen.blit(self.title, (50, 50))
+        super().update()
 
 if __name__ == "__main__":
     pygame.init()
